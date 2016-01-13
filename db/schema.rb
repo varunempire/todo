75f7bc21304713cf01199c96f3c27758c5832721
varunempire <varunempire@gmail.com>
2016-01-14 00:24:30 +0530

    Home Page Modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113151814) do
+ActiveRecord::Schema.define(version: 20160113181719) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
