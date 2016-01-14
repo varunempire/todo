a5c17ba93caf68fb3a3e940d158799d2514d6736
varunempire <varunempire@gmail.com>
2016-01-14 20:15:43 +0530

    Description Validation

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -13,7 +13,7 @@ describe "Creating Todo lists" do
 		expect(page).to have_content("My Todo list")
 	end
 
-	it "displays an error when Todo list has no title" do
+	it "displays an error when Todo list has no Title" do
 		expect(Todolist.count).to eq(0)
 
 		visit "/todo_lists"
@@ -28,6 +28,24 @@ describe "Creating Todo lists" do
 		expect(Todolist.count).to eq(0)
 
 		visit "/todo_lists"
-		expect(page).to have_content("not doing today")
+		expect(page).to_not have_content("not doing today")
+	end
+
+	it "displays an error when Todo list has no Description" do
+		expect(Todolist.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo List"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "grocery"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(Todolist.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("grocery")
 	end
 end
