132589ec3688a4a6aeae456232c078d572e3aa22
varunempire <varunempire@gmail.com>
2016-01-14 20:07:14 +0530

    Title Validation

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Creating Todo lists" do
+	it "redirects to the Todo list index page on success" do
+		visit "/todo_lists"
+		click_link "New Todo List"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "My Todo list"
+		fill_in "Description", with: "creating Todo lists"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("My Todo list")
+	end
+
+	it "displays an error when Todo list has no title" do
+		expect(Todolist.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo List"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: ""
+		fill_in "Description", with: "creating Todo lists"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(Todolist.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to have_content("not doing today")
+	end
+end
